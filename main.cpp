7d7af0cd4ffcb06abe570d633bfca647d99c441e
leopc17 <LeonardoEAF45@gmail.com>
2023-09-20 17:54:48 -0300

    finalização do projeto

--- main.cpp
+++ main.cpp
@@ -4,79 +4,176 @@ using namespace std;
 
 struct Conta {
     string nomeTitular;
-    int numConta;
+    int numeroConta;
     int senha;
     double saldo;
 };
 
-bool verificaSenha(Conta contas[], int senha, int posicao) {
-    if (contas[posicao].senha == senha) {
+int retornaIndice(Conta contas[], int quantidadeContas, int numeroConta) {
+    for (int i = 0; i < quantidadeContas; i++) {
+        if (contas[i].numeroConta == numeroConta) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+bool verificaSenha(Conta contas[], int senha, int indiceConta) {
+    if (contas[indiceConta].senha == senha) {
         return true;
     } else {
         return false;
     }
 }
 
-void criarConta(Conta contas[], int count) {
-    cout << "Informe o número da conta: " << endl;
-    cin >> contas[count].numConta;
-    cout << "Informe o nome do titular: " << endl;
-    cin >> contas[count].nomeTitular;
-    cout << "Informe uma senha de 4 digitos: " << endl;
-    cin >> contas[count].senha;
-    cout << "Informe o saldo inicial da conta: " << endl;
-    cin >> contas[count].saldo;
+void criarConta(Conta contas[], int quantidadeContas) {
+    cout << "Informe o número da conta: ";
+    cin >> contas[quantidadeContas].numeroConta;
+    cout << "Informe o nome do titular: ";
+    cin >> contas[quantidadeContas].nomeTitular;
+    cout << "Informe uma senha: ";
+    cin >> contas[quantidadeContas].senha;
+    cout << "Informe o saldo inicial da conta: ";
+    cin >> contas[quantidadeContas].saldo;
+    cout << endl;
 }
 
-double consultarSaldo(int numeroContas, Conta contas[]) {
-    int resposta, senha;
+void depositar(Conta contas[], int quantidadeContas) {
+    int numeroConta, senha;
+    double valor;
 
     cout << "Digite o número da conta: ";
-    cin >> resposta;
+    cin >> numeroConta;
 
-    for (int i = 0; i < numeroContas; i++) {
-        if (contas[i].numConta == resposta) {
-            cout << "Digite a senha: ";
-            cin >> senha;
+    int indiceConta = retornaIndice(contas, quantidadeContas, numeroConta);
 
-            while (!verificaSenha(contas, senha, i)) {
-                cout << "Senha errada, tente novamente: ";
-                cin >> senha;
-                verificaSenha(contas, senha, i);
-            }
+    cout << "Digite a senha: ";
+    cin >> senha;
 
-            return contas[i].saldo;
-        }
+    bool senhaCorreta = verificaSenha(contas, senha, indiceConta);
+
+    if (senhaCorreta) {
+        cout << "Digite o valor: ";
+        cin >> valor;
+
+        contas[indiceConta].saldo += valor;
+
+        cout << endl << "Você depositou: " << contas[indiceConta].saldo << " R$" << endl << endl;
+    } else {
+        cout << endl << "Senha incorreta." << endl << endl;
+    }
+}
+
+void sacar(Conta contas[], int quantidadeContas) {
+    int numeroConta, senha;
+    double valor;
+
+    cout << "Digite o número da conta: ";
+    cin >> numeroConta;
+
+    int indiceConta = retornaIndice(contas, quantidadeContas, numeroConta);
+
+    cout << "Digite a senha: ";
+    cin >> senha;
+
+    bool senhaCorreta = verificaSenha(contas, senha, indiceConta);
+
+    if (senhaCorreta) {
+        cout << "Digite o valor: ";
+        cin >> valor;
+
+        contas[indiceConta].saldo -= valor;
+
+        cout << endl << "Você sacou: " << contas[indiceConta].saldo << " R$" << endl << endl;
+    } else {
+        cout << endl << "Senha incorreta." << endl << endl;
+    }
+}
+
+void transferir(Conta contas[], int quantidadeContas) {
+    int numeroConta, senha, numeroContaDestino;
+
+    double valor;
+
+    cout << "Digite o número da conta: ";
+    cin >> numeroConta;
+
+    int indiceConta = retornaIndice(contas, quantidadeContas, numeroConta);
+
+    cout << "Digite a senha: ";
+    cin >> senha;
+
+    bool senhaCorreta = verificaSenha(contas, senha, indiceConta);
+
+    if (senhaCorreta) {
+        cout << "Digite a conta de destino: ";
+        cin >> numeroContaDestino;
+
+        cout << "Digite o valor: ";
+        cin >> valor;
+
+        int indiceContaDestino = retornaIndice(contas, quantidadeContas, numeroContaDestino);
+
+        contas[indiceContaDestino].saldo += valor;
+        contas[indiceConta].saldo -= valor;
+
+        cout << endl << "Você fez uma transferencia no valor de: " << valor << " R$" << endl << endl;
+    } else {
+        cout << endl << "Senha incorreta." << endl << endl;
+    }
+}
+
+void consultarSaldo(Conta contas[], int quantidadeContas) {
+    int numeroConta, senha;
+
+    cout << "Digite o número da conta: ";
+    cin >> numeroConta;
+
+    int indiceConta = retornaIndice(contas, quantidadeContas, numeroConta);
+
+    cout << "Digite a senha: ";
+    cin >> senha;
+
+    bool senhaCorreta = verificaSenha(contas, senha, indiceConta);
+
+    if (senhaCorreta) {
+        cout << endl << "Saldo da conta: " << contas[indiceConta].saldo << " R$" << endl << endl;
+    } else {
+        cout << endl << "Senha incorreta." << endl << endl;
     }
 }
 
 int main() {
-    Conta contas[50];
+    Conta *contas = new Conta[50];
 
     int escolha;
 
-    int numeroContas = 0;
+    int quantidadeContas = 0;
 
     do {
         cout << "Escolha uma das opções: " << endl;
         cout << "1. Criar conta" << endl;
         cout << "2. Depositar" << endl;
         cout << "3. Sacar" << endl;
-        cout << "4. Consultar saldo" << endl;
-        cout << "5. Sair" << endl;
+        cout << "4. Transferir" << endl;
+        cout << "5. Consultar saldo" << endl;
+        cout << "6. Sair" << endl;
         cin >> escolha;
 
         if (escolha == 1) {
-            criarConta(contas, numeroContas);
-            numeroContas++;
+            criarConta(contas, quantidadeContas);
+            quantidadeContas++;
         } else if (escolha == 2) {
-
+            depositar(contas, quantidadeContas);
         } else if (escolha == 3) {
-
+            sacar(contas, quantidadeContas);
         } else if (escolha == 4) {
-            cout << consultarSaldo(numeroContas, contas) << endl;
+            transferir(contas, quantidadeContas);
+        } else if (escolha == 5) {
+            consultarSaldo(contas, quantidadeContas);
         }
-    } while (escolha != 5);
+    } while (escolha != 6);
 
     return 0;
 }
